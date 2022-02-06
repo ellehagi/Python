55a58094f67b19bcc7919af8969828108835ed3f
Elham Hagi <elhamhagi@tests-MacBook-Air.local>
2022-02-06 21:25:45 +0000

    first commit

---
+++ pythonProject3/rock-paper-scissors.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won!'
+
+    return 'You lost!'
+
+def is_win(player, opponent):
+    # return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
+
+
+
+
